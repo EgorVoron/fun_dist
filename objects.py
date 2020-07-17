f790b86d1040abc134deb76e0acf9ae0a22bc4b9
EgorVoron <ekvoron01@gmail.com>
2020-07-17 20:47:23 +0300

    Add DistanceBetweenPointAndEllipse

--- objects.py
+++ objects.py
@@ -1,3 +1,6 @@
+from math import sin, cos, sqrt
+
+
 class Point:
     def __init__(self, x: float, y: float):
         self.x = x
@@ -8,6 +11,18 @@ class Circle:
     def __init__(self, center: Point, rad: float):
         self.center = center
         self.rad = rad
+
+
+class Ellipse:
+    def __init__(self, center: Point, a: float, b: float):
+        self.center = center
+        self.a = a
+        self.b = b
+
+        def ellipse_polar_func(theta):
+            return sqrt(1 / ((cos(theta) / a) ** 2 + (sin(theta) / b) ** 2))
+
+        self.polar_func = ellipse_polar_func
 
 
 class LinearFunction:
