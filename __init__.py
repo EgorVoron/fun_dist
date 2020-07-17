5dd300526227c26faebf1216d2d4788965994085
EgorVoron <ekvoron01@gmail.com>
2020-07-17 20:51:03 +0300

    Fix __all__

--- __init__.py
+++ __init__.py
@@ -2,8 +2,7 @@ from distances import *
 from objects import *
 from tangent import *
 
-
-__all__ = ['Point', 'Circle', 'LinearFunction',
+__all__ = ['Point', 'Circle', 'LinearFunction', 'Ellipse',
            'point2point', 'point2func', 'func2func',
-           'point2circle', 'func2circle', 'func_tangent',
-           'circle_tangent', 'circle_tangent_len']
+           'point2circle', 'point2ellipse', 'func_tangent', 'circle_tangent',
+           'circle_tangent_len']
